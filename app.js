90b87c4ac4c060d96ae8d1386d0121cbd0ab5f47
Nicolas Vera <57852440+queondatodotranqui@users.noreply.github.com>
2020-10-22 12:52:17 -0300

    Dia 1
    Hecho el prototipado, api fetcheada, datos recibidos

---
+++ app.js
@@ -0,0 +1,39 @@
+
+
+/*
+window.addEventListener('load', ()=>{
+    let long;
+    let lat;
+
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(position => {
+            long = position.coords.longitude;
+            lat = position.coords.latitude;
+
+            const key = 'ad5fd62fe26fee6134467fcefce7a6cf'
+            const proxy = 'https://cors-anywhere.herokuapp.com/'
+            let api = `${proxy}api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${key}`;
+
+            fetch(api)
+            .then(response =>{
+                return response.json();
+            })
+            .then(data =>{
+                console.log(data);
+
+                let {temp} = data.main;
+                console.log(temp);
+                
+                var tempCel = temperatureConverter(temp);
+                console.log(tempCel.toFixed(2));
+            })
+
+            function temperatureConverter(valNum) {
+                valNum = parseFloat(valNum);
+                var celcius=valNum-273.15;
+                return celcius;
+            }
+        });
+    }
+})
+*/
